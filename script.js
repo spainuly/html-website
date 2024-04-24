17f30a21d3ef6c32c2ada4da7894a350b998ffee
spainuly <spainuly234@gmail.com>
2024-04-24 11:14:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // You can add your form submission logic here
+    alert("Form submitted!");
+});
